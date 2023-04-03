79cc961d304ae176e92099a97804f0be961dc438
MaksimVasilev <126399905+MaksimVasilev@users.noreply.github.com>
2023-04-03 22:41:35 +0300

    HomeWork50/52

--- Program.cs
+++ Program.cs
@@ -1,42 +1,97 @@
-﻿// Задача 34. Задайте массив заполненный случайными положительными трехзначными числами
-// Напишите программу, которая покажет количство четных чисел в массиве
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
 
-void FillArray( int [] array)
-{
-    for ( int i = 0; i<array.Length; i++)
-    array[i] = new Random().Next(100,1000);
-}
-
-void PrintArray( int [] array)
-{
-    foreach (var item in array)
-    {
-        Console.Write($"{item} ");
-    }
-    Console.WriteLine();
-}
-
-int Count(int [] array)
-{  
-
+// void FillArray( int [,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//       for (int j = 0; j < array.GetLength(1); j++)
+//          array[i,j] = new Random().Next(1,10); 
+// }
+// void PrintArray( int [,] array)
+// {
+//     for ( int i = 0; i< array.GetLength(0); i++)
+//     {
+//     for (int j = 0; j < array.GetLength(1); j++)
+//     Console.Write($"{array[i,j],3}\t");
+//     Console.WriteLine();
+//     }
    
-    int count = 0;
-    for ( int i = 0; i<array.Length; i++)
-       if(array[i]%2==0)
-       {
-        count++;
-        
-       }
-    return count;
+// }
+
+// void Sum(int[,] array)
+// { 
+//     for (int j = 0; j < array.GetLength(1); j++)
+//     {
+//         double sum =0 ;
+//         for ( int i = 0; i< array.GetLength(0); i++)
+//         {
+//             sum+= array[i,j];
+//         }
+//         Console.Write($"{ sum / array.GetLength(0),3} ;");
+//     }
     
-}
+
+// }
+    
+
+// int [,] array = new int [4,4];
+// FillArray(array);
+// PrintArray(array);
+// Console.WriteLine("Среднее арифметическое элементов в каждом столбце равно:");
+// Sum(array);
+
+// -----------------------------------------------------------------
+
+
+// Задача 50. Напишите программу, которая на вход принимает значение элемента в двумерном массиве, и возвращает позицию этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+// int rows = ReadInt("Введите индекс строки: ");
+// int colums = ReadInt("Введите индекс столбца: ");
+// int[,] numbers = new int[3, 3];
+// FillArray(numbers);
+// PrintArray(numbers);
+
+// if (rows < numbers.GetLength(0) && colums < numbers.GetLength(1)) Console.WriteLine(numbers[rows, colums]);
+// else Console.WriteLine($"{rows} -> такого числа в массиве нет");
 
 
 
+// void FillArray(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             array[i, j] = new Random().Next(1, 10);
+//         }
+//     }
+// }
 
-int [] array = new int [4];
-FillArray(array);
-PrintArray(array);
-Console.WriteLine(Count(array));
+
+// void PrintArray(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.Write(array[i, j] + " ");
+//         }
+//         Console.WriteLine();
+//     }
+//     Console.WriteLine();
+// }
 
 
+// int ReadInt(string message)
+// {
+//     Console.Write(message);
+//     return Convert.ToInt32(Console.ReadLine());
+// }
